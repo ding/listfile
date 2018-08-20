452d2d5e444af11a08db9e55d2fd0b141137d1ec
Malvin <malvin.ding@outlook.com>
2018-08-20 16:58:44 +0800

    Add SHA256 parameter

--- listfile.go
+++ listfile.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"fmt"
 	"github.com/urfave/cli"
 	"io"
@@ -10,6 +11,15 @@ import (
 	"os"
 	"strings"
 )
+
+// Parameger struct
+type inputParameters struct {
+	splitStr    string
+	showSubname bool
+	showQuation bool
+	showMD5     bool
+	showSHA256  bool
+}
 
 func main() {
 	app := cli.NewApp()
@@ -41,7 +51,12 @@ func main() {
 		cli.BoolFlag{
 			Name:   "showmd5, m",
 			Hidden: false,
-			Usage:  "Output the file md5 code.",
+			Usage:  "Output the file's MD5 code.",
+		},
+		cli.BoolFlag{
+			Name:   "showsha256, a",
+			Hidden: false,
+			Usage:  "Output the file's SHA256 code.",
 		},
 	}
 	app.Action = mainAction
@@ -52,6 +67,8 @@ func main() {
 }
 
 func mainAction(c *cli.Context) error {
+	var inputParam inputParameters
+
 	inputPath := ""
 	if c.NArg() > 0 {
 		inputPath = c.Args().Get(0)
@@ -61,20 +78,20 @@ func mainAction(c *cli.Context) error {
 	}
 
 	// get the paramater
-	splitStr := c.String("split")
-	showSubname := c.Bool("showsubname")
-	showQuation := c.Bool("showquation")
-	showMD5 := c.Bool("showmd5")
+	inputParam.splitStr = c.String("split")
+	inputParam.showSubname = c.Bool("showsubname")
+	inputParam.showQuation = c.Bool("showquation")
+	inputParam.showMD5 = c.Bool("showmd5")
+	inputParam.showSHA256 = c.Bool("showsha256")
 
-	err := listFilesAndDirs(inputPath, splitStr, showSubname, showQuation, showMD5)
+	err := listFilesAndDirs(inputPath, inputParam)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func listFilesAndDirs(dirPath string, splitStr string,
-	showSubname bool, showQuation bool, showMD5 bool) error {
+func listFilesAndDirs(dirPath string, inputParam inputParameters) error {
 	dir, err := ioutil.ReadDir(dirPath)
 	if err != nil {
 		fmt.Println(err)
@@ -84,8 +101,8 @@ func listFilesAndDirs(dirPath string, splitStr string,
 
 	for _, fi := range dir {
 		if fi.IsDir() { // process dir
-			if showSubname {
-				if showQuation {
+			if inputParam.showSubname {
+				if inputParam.showQuation {
 					fmt.Println(surroundQuation(fi.Name()))
 				} else {
 					fmt.Println(fi.Name())
@@ -95,36 +112,47 @@ func listFilesAndDirs(dirPath string, splitStr string,
 				dirPath = dirPath + pathSep
 			}
 
-			err := listFilesAndDirs(dirPath+fi.Name(),
-				splitStr, showSubname, showQuation, showMD5)
+			err := listFilesAndDirs(dirPath+fi.Name(), inputParam)
 			if err != nil {
 				return err
 			}
 		} else { // process file
 			fileName := fi.Name()
 			currentPath := dirPath
-			if showQuation {
+			if inputParam.showQuation {
 				fileName = surroundQuation(fileName)
 				currentPath = surroundQuation(dirPath)
 			}
 			fmt.Print(currentPath)
-			fmt.Print(splitStr)
+			fmt.Print(inputParam.splitStr)
 			fmt.Print(fileName)
-			fmt.Print(splitStr)
+			fmt.Print(inputParam.splitStr)
 			fmt.Printf("%d", fi.Size())
-			fmt.Print(splitStr)
+			fmt.Print(inputParam.splitStr)
 			fmt.Printf("%v", fi.ModTime())
 
-			if showMD5 {
+			if inputParam.showMD5 {
 				md5String, err := md5sum(dirPath + pathSep + fi.Name())
 				if err != nil {
 					return err
 				}
-				if showQuation {
+				if inputParam.showQuation {
 					md5String = surroundQuation(md5String)
 				}
-				fmt.Print(splitStr)
+				fmt.Print(inputParam.splitStr)
 				fmt.Print(md5String)
+			}
+
+			if inputParam.showSHA256 {
+				sha256String, err := sha256sum(dirPath + pathSep + fi.Name())
+				if err != nil {
+					return err
+				}
+				if inputParam.showQuation {
+					sha256String = surroundQuation(sha256String)
+				}
+				fmt.Print(inputParam.splitStr)
+				fmt.Print(sha256String)
 			}
 			fmt.Printf("\n")
 		}
@@ -154,4 +182,23 @@ func md5sum(inputFile string) (string, error) {
 	returnMD5String = fmt.Sprintf("%x", hash.Sum(nil))
 
 	return returnMD5String, nil
+}
+
+func sha256sum(inputFile string) (string, error) {
+	var returnSHA256String string
+
+	file, err := os.Open(inputFile)
+	if err != nil {
+		return returnSHA256String, err
+	}
+	defer file.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return returnSHA256String, err
+	}
+
+	returnSHA256String = fmt.Sprintf("%x", hash.Sum(nil))
+
+	return returnSHA256String, nil
 }
